79d1468b5e47d4118a87b842803b40d6652bbffd
GiselePark <hanied04@gmail.com>
2024-03-14 20:07:20 +0900

    20240314 lambda expression of navigation

---
+++ test/com/dennis/api/product/item.java
@@ -0,0 +1,10 @@
+package com.dennis.api.product;
+
+public class item {
+    public String systemOut(){
+        return "Hello";
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ test/com/dennis/api/product/itemTest.java
@@ -0,0 +1,14 @@
+package com.dennis.api.product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class itemTest {
+
+    @org.junit.jupiter.api.Test
+    void systemOut() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+}
