c961b1ca8a3cd736c103384aec7dd395ff53a5c6
Juha Park <hanied04@gmail.com>
2024-03-17 22:46:25 +0900

    refactor - route

--- src/com/dennis/api/account/AccountView.java
+++ src/com/dennis/api/account/AccountView.java
@@ -4,9 +4,12 @@ import com.dennis.api.enums.AccountRouter;
 
 import java.util.Scanner;
 
+
 public class AccountView {
     public static void main(Scanner sc) {
+        AccountController accountController = new AccountController();
         AccountRouter.execute(sc);
 
+        while (AccountRouter.accountRoute(sc));
+        }
     }
-}
