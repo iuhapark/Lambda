0a6602b42dab7229338a4698a322b6dc0ef9ad2f
Juha Park <hanied04@gmail.com>
2024-03-17 22:44:28 +0900

    feat - builder

--- src/com/dennis/api/account/AccountController.java
+++ src/com/dennis/api/account/AccountController.java
@@ -1,15 +1,58 @@
 package com.dennis.api.account;
 
+import com.dennis.api.enums.Messenger;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class AccountController {
+    AccountServiceImpl account;
+    LocalDate localDate;
+
     private final static AccountController instance = new AccountController();
 
     private AccountController() {
-        service = AccountServiceImpl.getInstance();
     }
 
     public static AccountController getInstance() {
         return instance;
     }
+    public Messenger createAccount(Scanner sc){
+        return account.save(Account.builder()
+                .accountNumber(sc.next())
+                .accountHolder(sc.next())
+                .balance(sc.nextDouble())
+                .transactionDate(null)
+                .build()
+        );
+    }
 
-    private AccountServiceImpl service;
+    public String deposit(Scanner sc) {
+        return account.deposit(Account.builder()
+                .id(sc.nextLong())
+                .accountNumber(sc.next())
+                .accountHolder(sc.next())
+                .balance(sc.nextDouble())
+                .transactionDate(null)
+                .build());
+    }
+
+    public String withdraw(Scanner sc) {
+        return account.withdraw(Account.builder()
+                .id(sc.nextLong())
+                .accountNumber(sc.next())
+                .accountHolder(sc.next())
+                .balance(sc.nextDouble())
+                .transactionDate(null)
+                .build());
+    }
+
+    public String getBalance() {
+        return null;
+    }
+
+    public String delete(Scanner sc) {
+        return account.delete(Account.builder().accountNumber(sc.next()).build());
+    }
+}
 }
