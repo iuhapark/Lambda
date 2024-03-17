fb39eecdf92407dc098ccfa3e22ea470b7d78276
Juha Park <hanied04@gmail.com>
2024-03-17 22:46:05 +0900

    refactor - singleton pattern

--- src/com/dennis/api/account/AccountServiceImpl.java
+++ src/com/dennis/api/account/AccountServiceImpl.java
@@ -1,17 +1,12 @@
 package com.dennis.api.account;
 
-import com.dennis.api.menu.MenuRepository;
-
 public class AccountServiceImpl {
     private final static AccountServiceImpl instance = new AccountServiceImpl();
 
     private AccountServiceImpl() {
-        repo = AccountRepository.getInstance();
     }
 
     public static AccountServiceImpl getInstance() {
         return instance;
     }
-
-    private AccountRepository repo;
 }
